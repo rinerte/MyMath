1b94537a94babd64cf5cdd76ce22fada4ec90d36
rinerte <rinerte@gmail.com>
2024-08-04 21:24:00 +0900

    started with class for matrix

---
+++ MyMath/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MyMath
+{
+    public class Matrix
+    {
+        int rows;
+        int columns;
+        int[][] matrix;
+        public Matrix(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            matrix = new int[rows][];
+            for(int i=0;i<rows;i++) matrix[i] = new int[columns];
+        }
+        /// <summary>
+        /// Square matrix
+        /// </summary>
+        /// <param name="rows"></param>
+        public Matrix(int rows, bool identityMatrix = false) 
+        {
+            this.rows = rows;
+            this.rows = rows;
+            matrix = new int[rows][];
+            for (int i = 0; i < rows; i++) matrix[i] = new int[rows];
+
+            if (identityMatrix)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    matrix[i][i] = 1;
+                }
+            }
+        }
+        public int Rows { get { return rows; } }
+        public int Columns { get { return columns; } }        
+        public static Matrix operator + (Matrix a, Matrix b)
+        {
+            if (a.Columns != b.Columns || a.Rows != b.Rows) throw new ArgumentException(message: "Can not sum matrix with different size");
+
+            Matrix result = new Matrix(a.Rows, a.Columns);
+
+            for(int i = 0; i < a.Rows; i++)
+            {
+                for(int j = 0; j < a.Columns; j++)
+                {
+                    result[i][j] = a[i][j] + b[i][j];
+                }
+            }
+            return result;
+        } 
+        public int[] this[int i]
+        {
+            get => matrix[i];
+            set => matrix[i] = value;
+        }
+
+
+    }
+}
